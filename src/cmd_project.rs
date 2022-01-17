7de79648c93adf09b7140e031497f250d62344f2
Jess Frazelle <github@jessfraz.com>
2022-01-17 11:28:48 -0800

    start of list
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/cmd_project.rs
+++ src/cmd_project.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 use anyhow::{anyhow, Result};
 use clap::Parser;
 
@@ -113,13 +115,87 @@ impl crate::cmd::Command for CmdProjectEdit {
 }
 
 /// List projects owned by user or organization.
+///
+/// If no organization is specified, the user's projects are listed.
+///
+/// In `--paginate` mode, all pages of results will sequentially be requested until
+/// there are no more pages of results.
 #[derive(Parser, Debug, Clone)]
 #[clap(verbatim_doc_comment)]
-pub struct CmdProjectList {}
+pub struct CmdProjectList {
+    /// The name of the organization to list projects for.
+    #[clap(name = "organization", required = false)]
+    pub organization: String,
+
+    /// Maximum number of projects to list.
+    #[clap(long, short, default_value = "30")]
+    pub limit: u32,
+
+    /// Make additional HTTP requests to fetch all pages of projects.
+    #[clap(long)]
+    pub paginate: bool,
+
+    /// Output JSON.
+    #[clap(long)]
+    pub json: bool,
+}
 
 #[async_trait::async_trait]
 impl crate::cmd::Command for CmdProjectList {
-    async fn run(&self, _ctx: &mut crate::context::Context) -> Result<()> {
+    async fn run(&self, ctx: &mut crate::context::Context) -> Result<()> {
+        if self.limit < 1 {
+            return Err(anyhow!("--limit must be greater than 0"));
+        }
+
+        let client = ctx.api_client("")?;
+
+        // TODO: Make this work for a user's projects.
+        let projects = if self.paginate {
+            client
+                .projects()
+                .get_all(oxide_api::types::NameSortMode::NameAscending, &self.organization)
+                .await?
+        } else {
+            client
+                .projects()
+                .get_page(
+                    self.limit,
+                    "",
+                    oxide_api::types::NameSortMode::NameAscending,
+                    &self.organization,
+                )
+                .await?
+        };
+
+        if self.json {
+            // If they specified --json, just dump the JSON.
+            ctx.io.write_json(&serde_json::json!(projects))?;
+            return Ok(());
+        }
+
+        let cs = ctx.io.color_scheme();
+
+        let mut tw = tabwriter::TabWriter::new(vec![]);
+        writeln!(tw, "NAME\tDESCRTIPTION\tLAST UPDATED")?;
+        for project in projects {
+            // TODO: There should be a way to do this without so many http requests.
+            let org = client.organizations().get(&project.organization_id).await?;
+            let full_name = format!("{}/{}", org.name, project.name);
+
+            let last_updated = chrono::Utc::now() - project.time_modified.unwrap_or(project.time_created.unwrap());
+            writeln!(
+                tw,
+                "{}\t{}\t{}",
+                cs.bold(&full_name),
+                cs.gray(&project.description),
+                chrono_humanize::HumanTime::from(last_updated)
+            )?;
+        }
+        tw.flush()?;
+
+        let table = String::from_utf8(tw.into_inner()?)?;
+        writeln!(ctx.io.out, "{}", table)?;
+
         Ok(())
     }
 }
