eae7370e4ebd6dd2b2d7828fdd8661f28800835b
Jess Frazelle <github@jessfraz.com>
2022-03-16 23:17:49 -0700

    update
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/cmd_project.rs
+++ src/cmd_project.rs
@@ -436,3 +436,165 @@ impl crate::cmd::Command for CmdProjectView {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use pretty_assertions::assert_eq;
+    use test_context::{test_context, AsyncTestContext};
+
+    use crate::cmd::Command;
+
+    pub struct TestItem {
+        name: String,
+        cmd: crate::cmd_project::SubCommand,
+        stdin: String,
+        want_out: String,
+        want_err: String,
+    }
+
+    struct TContext {
+        orig_oxide_host: Result<String, std::env::VarError>,
+        orig_oxide_token: Result<String, std::env::VarError>,
+    }
+
+    #[async_trait::async_trait]
+    impl AsyncTestContext for TContext {
+        async fn setup() -> TContext {
+            let orig = TContext {
+                orig_oxide_host: std::env::var("OXIDE_HOST"),
+                orig_oxide_token: std::env::var("OXIDE_TOKEN"),
+            };
+
+            // Set our test values.
+            let test_host = std::env::var("OXIDE_TEST_HOST").unwrap_or_default().to_string();
+            let test_token = std::env::var("OXIDE_TEST_TOKEN").unwrap_or_default().to_string();
+            std::env::set_var("OXIDE_HOST", test_host);
+            std::env::set_var("OXIDE_TOKEN", test_token);
+
+            orig
+        }
+
+        async fn teardown(self) {
+            // Put the original env var back.
+            if let Ok(ref val) = self.orig_oxide_host {
+                std::env::set_var("OXIDE_HOST", val);
+            } else {
+                std::env::remove_var("OXIDE_HOST");
+            }
+
+            if let Ok(ref val) = self.orig_oxide_token {
+                std::env::set_var("OXIDE_TOKEN", val);
+            } else {
+                std::env::remove_var("OXIDE_TOKEN");
+            }
+        }
+    }
+
+    #[test_context(TContext)]
+    #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+    async fn test_cmd_project() {
+        let tests: Vec<TestItem> = vec![
+            TestItem {
+                name: "create no name".to_string(),
+                cmd: crate::cmd_project::SubCommand::Create(crate::cmd_project::CmdProjectCreate {
+                    project: "".to_string(),
+                    organization: "".to_string(),
+                    description: "".to_string(),
+                }),
+
+                stdin: "".to_string(),
+                want_out: "".to_string(),
+                want_err: "[project_name] required in non-interactive mode".to_string(),
+            },
+            TestItem {
+                name: "create no organization".to_string(),
+                cmd: crate::cmd_project::SubCommand::Create(crate::cmd_project::CmdProjectCreate {
+                    project: "things".to_string(),
+                    organization: "".to_string(),
+                    description: "".to_string(),
+                }),
+
+                stdin: "".to_string(),
+                want_out: "".to_string(),
+                want_err: "--organization,-o required in non-interactive mode".to_string(),
+            },
+            TestItem {
+                name: "create no description".to_string(),
+                cmd: crate::cmd_project::SubCommand::Create(crate::cmd_project::CmdProjectCreate {
+                    project: "things".to_string(),
+                    organization: "foo".to_string(),
+                    description: "".to_string(),
+                }),
+
+                stdin: "".to_string(),
+                want_out: "".to_string(),
+                want_err: "--description,-D required in non-interactive mode".to_string(),
+            },
+            TestItem {
+                name: "delete no --confirm non-interactive".to_string(),
+                cmd: crate::cmd_project::SubCommand::Delete(crate::cmd_project::CmdProjectDelete {
+                    project: "things".to_string(),
+                    organization: "".to_string(),
+                    confirm: false,
+                }),
+
+                stdin: "".to_string(),
+                want_out: "".to_string(),
+                want_err: "--confirm required when not running interactively".to_string(),
+            },
+            TestItem {
+                name: "list zero limit".to_string(),
+                cmd: crate::cmd_project::SubCommand::List(crate::cmd_project::CmdProjectList {
+                    limit: 0,
+                    organization: "".to_string(),
+                    paginate: false,
+                    json: false,
+                }),
+
+                stdin: "".to_string(),
+                want_out: "".to_string(),
+                want_err: "--limit must be greater than 0".to_string(),
+            },
+        ];
+
+        let mut config = crate::config::new_blank_config().unwrap();
+        let mut c = crate::config_from_env::EnvConfig::inherit_env(&mut config);
+
+        for t in tests {
+            let (mut io, stdout_path, stderr_path) = crate::iostreams::IoStreams::test();
+            if !t.stdin.is_empty() {
+                io.stdin = Box::new(std::io::Cursor::new(t.stdin));
+            }
+            // We need to also turn off the fancy terminal colors.
+            // This ensures it also works in GitHub actions/any CI.
+            io.set_color_enabled(false);
+            io.set_never_prompt(true);
+            let mut ctx = crate::context::Context {
+                config: &mut c,
+                io,
+                debug: false,
+            };
+
+            let cmd_project = crate::cmd_project::CmdProject { subcmd: t.cmd };
+            match cmd_project.run(&mut ctx).await {
+                Ok(()) => {
+                    let stdout = std::fs::read_to_string(stdout_path).unwrap();
+                    let stderr = std::fs::read_to_string(stderr_path).unwrap();
+                    assert!(stderr.is_empty(), "test {}: {}", t.name, stderr);
+                    if !stdout.contains(&t.want_out) {
+                        assert_eq!(stdout, t.want_out, "test {}: stdout mismatch", t.name);
+                    }
+                }
+                Err(err) => {
+                    let stdout = std::fs::read_to_string(stdout_path).unwrap();
+                    let stderr = std::fs::read_to_string(stderr_path).unwrap();
+                    assert_eq!(stdout, t.want_out, "test {}", t.name);
+                    if !err.to_string().contains(&t.want_err) {
+                        assert_eq!(err.to_string(), t.want_err, "test {}: err mismatch", t.name);
+                    }
+                    assert!(stderr.is_empty(), "test {}: {}", t.name, stderr);
+                }
+            }
+        }
+    }
+}
