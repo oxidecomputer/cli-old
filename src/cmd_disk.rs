e5f6a901aff772437738cd00e762f85e37ebb403
Jess Frazelle <github@jessfraz.com>
2022-03-16 23:24:19 -0700

    add disk
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/cmd_disk.rs
+++ src/cmd_disk.rs
@@ -139,16 +139,20 @@ impl crate::cmd::Command for CmdDiskCreate {
         let mut size = self.size;
         let mut snapshot = self.snapshot.to_string();
 
-        if project_name.is_empty() && !ctx.io.can_prompt() {
-            return Err(anyhow!("--project,-p required in non-interactive mode"));
+        if disk_name.is_empty() && !ctx.io.can_prompt() {
+            return Err(anyhow!("[disk_name] required in non-interactive mode"));
         }
 
         if organization.is_empty() && !ctx.io.can_prompt() {
             return Err(anyhow!("--organization,-o required in non-interactive mode"));
         }
 
-        if disk_name.is_empty() && !ctx.io.can_prompt() {
-            return Err(anyhow!("[disk_name] required in non-interactive mode"));
+        if project_name.is_empty() && !ctx.io.can_prompt() {
+            return Err(anyhow!("--project,-p required in non-interactive mode"));
+        }
+
+        if description.is_empty() && !ctx.io.can_prompt() {
+            return Err(anyhow!("--description,-D required in non-interactive mode"));
         }
 
         // TODO: Add this back when snapshots are supported.
@@ -158,10 +162,6 @@ impl crate::cmd::Command for CmdDiskCreate {
 
         if size == 0 && !ctx.io.can_prompt() {
             return Err(anyhow!("--size,-s required in non-interactive mode"));
-        }
-
-        if description.is_empty() && !ctx.io.can_prompt() {
-            return Err(anyhow!("--description,-D required in non-interactive mode"));
         }
 
         // If they didn't specify an organization, prompt for it.
@@ -593,5 +593,168 @@ impl crate::cmd::Command for CmdDiskView {
         writeln!(ctx.io.out, "{}", table)?;
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use pretty_assertions::assert_eq;
+
+    use crate::cmd::Command;
+
+    pub struct TestItem {
+        name: String,
+        cmd: crate::cmd_disk::SubCommand,
+        stdin: String,
+        want_out: String,
+        want_err: String,
+    }
+
+    #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+    async fn test_cmd_disk() {
+        let tests: Vec<TestItem> = vec![
+            TestItem {
+                name: "create no name".to_string(),
+                cmd: crate::cmd_disk::SubCommand::Create(crate::cmd_disk::CmdDiskCreate {
+                    disk: "".to_string(),
+                    organization: "".to_string(),
+                    project: "".to_string(),
+                    description: "".to_string(),
+                    size: Default::default(),
+                    snapshot: Default::default(),
+                }),
+
+                stdin: "".to_string(),
+                want_out: "".to_string(),
+                want_err: "[disk_name] required in non-interactive mode".to_string(),
+            },
+            TestItem {
+                name: "create no organization".to_string(),
+                cmd: crate::cmd_disk::SubCommand::Create(crate::cmd_disk::CmdDiskCreate {
+                    disk: "things".to_string(),
+                    organization: "".to_string(),
+                    project: "".to_string(),
+                    description: "".to_string(),
+                    size: Default::default(),
+                    snapshot: Default::default(),
+                }),
+
+                stdin: "".to_string(),
+                want_out: "".to_string(),
+                want_err: "--organization,-o required in non-interactive mode".to_string(),
+            },
+            TestItem {
+                name: "create no project".to_string(),
+                cmd: crate::cmd_disk::SubCommand::Create(crate::cmd_disk::CmdDiskCreate {
+                    disk: "things".to_string(),
+                    organization: "foo".to_string(),
+                    project: "".to_string(),
+                    description: "".to_string(),
+                    size: Default::default(),
+                    snapshot: Default::default(),
+                }),
+
+                stdin: "".to_string(),
+                want_out: "".to_string(),
+                want_err: "--project,-p required in non-interactive mode".to_string(),
+            },
+            TestItem {
+                name: "create no description".to_string(),
+                cmd: crate::cmd_disk::SubCommand::Create(crate::cmd_disk::CmdDiskCreate {
+                    disk: "things".to_string(),
+                    organization: "foo".to_string(),
+                    project: "bar".to_string(),
+                    description: "".to_string(),
+                    size: Default::default(),
+                    snapshot: Default::default(),
+                }),
+
+                stdin: "".to_string(),
+                want_out: "".to_string(),
+                want_err: "--description,-D required in non-interactive mode".to_string(),
+            },
+            TestItem {
+                name: "create no size".to_string(),
+                cmd: crate::cmd_disk::SubCommand::Create(crate::cmd_disk::CmdDiskCreate {
+                    disk: "things".to_string(),
+                    organization: "foo".to_string(),
+                    project: "bar".to_string(),
+                    description: "blah blah".to_string(),
+                    size: Default::default(),
+                    snapshot: Default::default(),
+                }),
+
+                stdin: "".to_string(),
+                want_out: "".to_string(),
+                want_err: "--size,-s required in non-interactive mode".to_string(),
+            },
+            TestItem {
+                name: "delete no --confirm non-interactive".to_string(),
+                cmd: crate::cmd_disk::SubCommand::Delete(crate::cmd_disk::CmdDiskDelete {
+                    disk: "things".to_string(),
+                    organization: "".to_string(),
+                    project: "".to_string(),
+                    confirm: false,
+                }),
+
+                stdin: "".to_string(),
+                want_out: "".to_string(),
+                want_err: "--confirm required when not running interactively".to_string(),
+            },
+            TestItem {
+                name: "list zero limit".to_string(),
+                cmd: crate::cmd_disk::SubCommand::List(crate::cmd_disk::CmdDiskList {
+                    limit: 0,
+                    organization: "".to_string(),
+                    project: "".to_string(),
+                    paginate: false,
+                    json: false,
+                }),
+
+                stdin: "".to_string(),
+                want_out: "".to_string(),
+                want_err: "--limit must be greater than 0".to_string(),
+            },
+        ];
+
+        let mut config = crate::config::new_blank_config().unwrap();
+        let mut c = crate::config_from_env::EnvConfig::inherit_env(&mut config);
+
+        for t in tests {
+            let (mut io, stdout_path, stderr_path) = crate::iostreams::IoStreams::test();
+            if !t.stdin.is_empty() {
+                io.stdin = Box::new(std::io::Cursor::new(t.stdin));
+            }
+            // We need to also turn off the fancy terminal colors.
+            // This ensures it also works in GitHub actions/any CI.
+            io.set_color_enabled(false);
+            io.set_never_prompt(true);
+            let mut ctx = crate::context::Context {
+                config: &mut c,
+                io,
+                debug: false,
+            };
+
+            let cmd_disk = crate::cmd_disk::CmdDisk { subcmd: t.cmd };
+            match cmd_disk.run(&mut ctx).await {
+                Ok(()) => {
+                    let stdout = std::fs::read_to_string(stdout_path).unwrap();
+                    let stderr = std::fs::read_to_string(stderr_path).unwrap();
+                    assert!(stderr.is_empty(), "test {}: {}", t.name, stderr);
+                    if !stdout.contains(&t.want_out) {
+                        assert_eq!(stdout, t.want_out, "test {}: stdout mismatch", t.name);
+                    }
+                }
+                Err(err) => {
+                    let stdout = std::fs::read_to_string(stdout_path).unwrap();
+                    let stderr = std::fs::read_to_string(stderr_path).unwrap();
+                    assert_eq!(stdout, t.want_out, "test {}", t.name);
+                    if !err.to_string().contains(&t.want_err) {
+                        assert_eq!(err.to_string(), t.want_err, "test {}: err mismatch", t.name);
+                    }
+                    assert!(stderr.is_empty(), "test {}: {}", t.name, stderr);
+                }
+            }
+        }
     }
 }
