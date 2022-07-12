f18c8d304a4ac9603c40b2c9aff94c08a13534a7
Ryan England <rcengland@gmail.com>
2022-07-12 18:00:45 -0400

    Change description for format parameter (#242)

--- cli-macro-impl/src/lib.rs
+++ cli-macro-impl/src/lib.rs
@@ -1664,7 +1664,7 @@ impl Operation {
                 #[clap(short, long)]
                 pub web: bool,
 
-                /// Output format.
+                /// Display output in json, yaml, or table format.
                 #[clap(long, short)]
                 pub format: Option<crate::types::FormatOutput>,
             }
@@ -1778,7 +1778,7 @@ impl Operation {
                 #[clap(long)]
                 pub paginate: bool,
 
-                /// Output format.
+                /// Display output in json, yaml, or table format.
                 #[clap(long, short)]
                 pub format: Option<crate::types::FormatOutput>,
             }
